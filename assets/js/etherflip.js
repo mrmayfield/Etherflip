f12ce92cd482c60b58c04c8e9117e9fbf890ee1b
james britt <jambtt@gmail.com>
2016-03-14 15:23:46 +1100

    mist browser compatible

--- assets/js/etherflip.js
+++ assets/js/etherflip.js
@@ -1,10 +1,8 @@
 
 if(typeof web3 !== 'undefined')
   web3 = new Web3(web3.currentProvider);
-else{
-  web3 = new Web3();
-  web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-}
+else
+  web3 = new Web3(new web3.providers.HttpProvider("http://localhost:8545"));
 
 
 $(document).ready(function() {
